a00a52f75eb7584d53c7335cded49673c657f301
Orleans County ITS <137132070+OrleansCountyNY@users.noreply.github.com>
2023-07-03 11:11:28 -0400

    Update and rename get_logged_on_users.py to get_end_users.py

--- examples/get_end_users.py
+++ examples/get_end_users.py
@@ -9,10 +9,11 @@ client = ninjarmmpy.Client(
     SecretAccessKey=os.environ.get('NRMM_SECRET'),
     Europe=False
 )
+organizationId = 'YOUR_ORG_ID'
 # Get usernames and logon times for all devices as Python dictionaries
-logged_on_users = client.get_logged_on_users()
+end_users = client.getEndUsers(organizationId)
 # For this example, we're just going to convert the dictionaries to JSON and write them to a file.
-logged_on_users = json.dumps(logged_on_users)
+end_users = json.dumps(end_users)
 # Now we can write the results to a JSON file.
 with open('logged_on_users.json', 'w', newline='') as f:
-    print(logged_on_users, file=f)
+    print(end_users, file=f)
